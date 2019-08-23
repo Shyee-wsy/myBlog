834fdab8f1801ec1dd4a0c642ba34fdfb95a5bcc
Shyee-wsy <shyee-wsy@outlook.com>
2019-08-23 23:31:50 +0800

    增加图标

--- src/components/layout/components/sidebar.vue
+++ src/components/layout/components/sidebar.vue
@@ -2,7 +2,10 @@
   <div>
     <el-card shadow="hover">
       <el-menu>
-        <el-menu-item v-for="item in constantRouterMap">{{item.content}}</el-menu-item>
+        <el-menu-item v-for="item in constantRouterMap">
+          <i :class="item.icon"></i>
+          {{item.content}}
+        </el-menu-item>
       </el-menu>
     </el-card>
   </div>
@@ -13,9 +16,11 @@
     data() {
       return {
         constantRouterMap: [
-          {id: 1, content: '文章'},
-          { id: 2, content: '社交'},
-          { id: 3, content: '开源项目'},
+          {id: 1, content: '文章', icon: 'el-icon-notebook-2'},
+          { id: 2, content: '社交', icon: 'el-icon-user'},
+          { id: 3, content: '开源项目', icon: 'el-icon-s-grid'},
+          { id: 4, content: '评论', icon: 'el-icon-chat-dot-round'},
+          { id: 5, content: '关于作者', icon: 'el-icon-user-solid'}
         ]
       }
     }
