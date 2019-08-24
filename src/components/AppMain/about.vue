b56179124a4f42b7281f3cac37d53537a8e6235b
Shyee-wsy <shyee-wsy@outlook.com>
2019-08-24 23:25:17 +0800

    新增关于作者

---
+++ src/components/AppMain/about.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <el-card shadow="never">
+      <div class="about">
+        <img src="https://avatars2.githubusercontent.com/u/39408629?v=4" alt="头像">
+        <div>
+          <p><b>基本信息</b></p>
+          <ul>
+            <li>姓名: XXX</li>
+            <li>籍贯: 成都</li>
+            <li>职业: 攻城狮</li>
+            <li>出生日期: 2019年10月10日</li>
+            <li>兴趣: 吃饭睡觉打豆豆</li>
+          </ul>
+          <p><b>联系作者</b></p>
+          <ul>
+            <li>Email: XXX@email.com</li>
+          </ul>
+        </div>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  img{
+    float: left;
+    width: 150px;
+    height: 150px;
+    margin-right: 80px;
+  }
+  .about{
+    display: flex;
+    padding-left: 100px;
+    font-size: 1.2rem;
+    line-height: 1.5em;
+  }
+p{
+  margin-top: 20px;
+}
+</style>
