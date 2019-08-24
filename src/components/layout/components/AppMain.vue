713c72293384e2e048da79c3ea6e6d6ee3a7e643
Shyee-wsy <shyee-wsy@outlook.com>
2019-08-24 23:27:14 +0800

    新增appmain

--- src/components/layout/components/AppMain.vue
+++ src/components/layout/components/AppMain.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
-    <el-card>
-      main
+    <el-card shadow="hover">
+      <!--<router-view/>-->
+      <!--<social></social>-->
+      <about></about>
     </el-card>
   </div>
 </template>
+
+<script>
+import social from '../.././AppMain/social'
+import about from '../.././AppMain/about'
+  export default {
+    components: {
+      social,
+      about
+    },
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
