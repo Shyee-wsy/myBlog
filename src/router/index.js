df0d5eae5a9559a7596b134ae051ce16df3309ef
Shyee-wsy <shyee-wsy@outlook.com>
2019-08-24 23:26:52 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Layout from '@/components/layout/Layout'
+// import blog from '@/components/AppMain/blog'
+// import social from '@/components/AppMain/social'
+// import about from '@/components/AppMain/about'
 
 Vue.use(Router)
 
@@ -8,8 +11,60 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'main',
+      // redirect: '/user/blog/main',
       component: Layout
+      // children: {
+      //   path: 'main',
+      //   component: blog,
+      // }
+    // },
+    // {
+    //   path: '/user/social',
+    //   component: Layout,
+    //   children: {
+    //     path: 'main',
+    //     component: social
+    //   }
+    // },
+    // {
+    //   path: '/user/about',
+    //   component: Layout,
+    //   children: {
+    //     path: 'main',
+    //     component: about
+    //   }
     }
   ]
 })
+
+// const RouterMap = [
+//   {
+//     path: '/',
+//     redirect: '/user/blog/main',
+//     component: Layout,
+//     meta: {
+//       title: '博客'
+//     },
+//     children: {
+//       path: 'main',
+//       component: blog,
+//     }
+//   },
+//   {
+//     path: '/user/social',
+//     component: Layout,
+//     children: {
+//       path: 'main',
+//       component: social
+//     }
+//   },
+//   {
+//     path: '/user/about',
+//     component: Layout,
+//     children: {
+//       path: 'main',
+//       component: about
+//     }
+//   }
+// ]
+
