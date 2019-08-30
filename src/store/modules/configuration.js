5f6bf61d627f7273e805926dc2c71b350321a6e2
Shyee-wsy <shyee-wsy@outlook.com>
2019-08-30 23:19:02 +0800

    设置信息

---
+++ src/store/modules/configuration.js
@@ -0,0 +1,34 @@
+const configuration = {
+  state: {
+    githubUsername: '',
+    htmlTile: '',
+  },
+
+  mutations: {
+    SET_CONFIGURATION: (state, configuration) => {
+      state.githubUsername = configuration['githubUsername'] || 'Shyee-wsy'
+      state.htmlTile = configuration['htmlTitle'] || `${state.githubUsername}的博客`
+    }
+  },
+
+  actions: {
+    LocalReload ({ commit }, configuration) {
+      commit('SET_CONFIGURATION', configuration)
+    },
+    Init( { commit }){
+      let xmlhttp
+      //判断浏览器是否支持XHR对象
+      if(window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest()
+      } else {
+        // IE7之前的版本
+        xmlhttp = new ActiveXObject('Microsoft.XMLHTTP')
+      }
+      xmlhttp.send()
+      let configuration = JSON.parse(xmlhttp.responseText)
+      commit('SET_CONFIGURATION', configuration)
+    }
+  }
+}
+
+export default configuration
