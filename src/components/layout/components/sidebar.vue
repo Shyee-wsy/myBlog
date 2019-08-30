a9499886288dddf72b85fcd8846ab8e7051487d1
Shyee-wsy <shyee-wsy@outlook.com>
2019-08-30 23:21:04 +0800

    token设置弹窗

--- src/components/layout/components/sidebar.vue
+++ src/components/layout/components/sidebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <el-card shadow="hover"  style="height: 75vh;">
+    <el-card shadow="hover">
       <el-menu @select="onSelect">
         <el-menu-item v-for="item in RouterMap" :key="item.path" :index="item.path">
           <i :class="item.meta.icon"></i>
@@ -8,21 +8,42 @@
         </el-menu-item>
       </el-menu>
     </el-card>
+
+    <el-card style="margin-top:15px;" shadow="hover">
+      <span>&nbsp;&nbsp;Token未绑定&nbsp;&nbsp;</span>
+      <el-button type="text" @click="openTokenDialog">绑定</el-button>
+    </el-card>
+    <token-dialog ref="tokenDialog"></token-dialog>
   </div>
 </template>
 
 <script>
   import { RouterMap } from '@/router/index'
+  import tokenDialog from './tokenDialog'
+  import { mapGetters } from 'vuex'
+
   export default {
+    components: {
+      tokenDialog
+    },
     data() {
       return {
         RouterMap
       }
     },
+    computed: {
+      ...mapGetters([
+        'token',
+        'githubUsername'
+      ])
+    },
     methods: {
       onSelect(index) {
         this.$router.push(index)
+      },
+      openTokenDialog(){
+        this.$refs.tokenDialog.open(() => {})
       }
-    }
+    },
   }
 </script>
