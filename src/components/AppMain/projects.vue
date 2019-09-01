4ee1990f140ce140576f849f57aa56579d21e867
Shyee-wsy <shyee-wsy@outlook.com>
2019-09-01 22:22:04 +0800

    项目动态加载

--- src/components/AppMain/projects.vue
+++ src/components/AppMain/projects.vue
@@ -1,5 +1,57 @@
 <template>
   <div>
-    <h1>projects</h1>
+    <div v-if="projects.length" v-loading="loading">
+      <el-row>
+        <el-col v-for="item in projects" :key="item.id" :span="12" style="padding: 10px">
+          <el-card>
+            <div slot="header" style="font-size: 1.3rem;color: #409EFF">
+              <i class="el-icon-folder"></i>
+              <a :href="item.url" target="_blank" style="text-decoration: none; color: inherit">{{item.project_name}}</a>
+            </div>
+            <div>
+              <time style="font-size: 0.8rem;color: darkred">上次更新: {{ item.update}}</time>
+              <div v-if="item.description !== null" style="margin-top:20px;">项目介绍: {{item.description}}</div>
+              <div v-else style="margin-top:20px;">无项目介绍</div>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+    <div v-else>暂时没有项目:)</div>
   </div>
 </template>
+
+<script>
+import user from '@/api/user'
+  export default {
+    data(){
+      return {
+        projects: [],
+        loading: false
+      }
+    },
+    methods: {
+      fetchProjects() {
+        this.loading = true
+        user.projects().then(data => {
+          console.log(data)
+          for(let i = 0; i < data.length; i++) {
+            this.projects.push({
+              id: data[i].id,
+              project_name: data[i].name,
+              description: data[i].description,
+              update: data[i].updated_at,
+              watcher: data[i].watchers_count,
+              forks: data[i].forks_count,
+              language: data[i].language,
+              url: data[i].clone_url
+            })
+          }
+        }).then( () => this.loading=false)
+      }
+    },
+    mounted(){
+      this.fetchProjects();
+    }
+  }
+</script>
