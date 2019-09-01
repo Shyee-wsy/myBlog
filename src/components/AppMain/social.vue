a1ecc84e69c61afec424efb7717c0adb50d844da
Shyee-wsy <shyee-wsy@outlook.com>
2019-09-01 16:49:45 +0800

    社交动态加载

--- src/components/AppMain/social.vue
+++ src/components/AppMain/social.vue
@@ -4,14 +4,14 @@
         <el-tabs>
           <el-tab-pane label="关注">
             <div v-if="following.length">
-              <el-row style="min-height: 200px;">
+              <el-row style="min-height: 200px;" :gutter="20">
                 <el-col v-for="item in following" :key="item.id" :span="6" style="padding:10px;">
                   <el-card shadow="hover" style="font-size:16px;line-height: 20px;">
                     <i class="el-icon-star-off"></i>
                     <a>{{item.name}}</a><br/>
                     <i class="el-icon-message"></i>
-                    <a>TA的主页</a><br/>
-                    <img>
+                    <a :href="item.url" target="_blank" style="text-decoration: none; color: deepskyblue;">TA的主页</a><br/>
+                    <img :src="item.avatar" style="width:100%;height:100%">
                   </el-card>
                 </el-col>
               </el-row>
@@ -29,7 +29,7 @@
                     <i class="el-icon-star-off"></i>
                     <a>{{item.name}}</a><br/>
                     <i class="el-icon-message"></i>
-                    <a>TA的主页</a><br/>
+                    <a :href="item.url" target="_blank" style="text-decoration: none; color: deepskyblue;">TA的主页</a><br/>
                     <img :src="item.avatar" style="width:100%;height: 100%">
                   </el-card>
                 </el-col>
@@ -45,28 +45,35 @@
 </template>
 
 <script>
-import axios from 'axios'
+import user from '@/api/user'
+
   export default {
     data() {
       return {
         follower:[],
-        following: [
-          {id:0, name: 'sun'}
-        ]
+        following: []
       }
     },
     methods:{
       fetchFollower(){
-        axios.get('https://api.github.com/users/Shyee-wsy/following').then(resp => {
-          let data = resp.data
-          let name = data[0].login
-          this.follower.push({id: 0, name: name + '', avatar: 'https://avatars3.githubusercontent.com/u/20197018?v=4'})
+        user.followers().then(data => {
+          for(let i = 0; i < data.length; i++){
+            this.follower.push({id: data[i].id, name: data[i].login, avatar: data[i].avatar_url, url: data[i].html_url})
+          }
+        })
+      },
+      fetchFollowing(){
+        user.following().then(data =>{
+          console.log(data)
+          for(let i = 0; i < data.length; i++){
+            this.following.push({id: data[i].id, name: data[i].login, avatar: data[i].avatar_url, url: data[i].html_url})
+          }
         })
       }
-
     },
     mounted(){
       this.fetchFollower();
+      this.fetchFollowing();
     }
   }
 </script>
