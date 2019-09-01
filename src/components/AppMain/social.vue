633fef765e85186b95b03c42e2f946ebb474a620
Shyee-wsy <shyee-wsy@outlook.com>
2019-09-01 22:23:01 +0800

    添加loading

--- src/components/AppMain/social.vue
+++ src/components/AppMain/social.vue
@@ -2,16 +2,16 @@
   <div>
       <template>
         <el-tabs>
-          <el-tab-pane label="关注">
-            <div v-if="following.length">
+          <el-tab-pane :label="'关注 ' + following.total">
+            <div v-if="following.list.length" v-loading="following.loading">
               <el-row style="min-height: 200px;" :gutter="20">
-                <el-col v-for="item in following" :key="item.id" :span="6" style="padding:10px;">
+                <el-col v-for="item in following.list" :key="item.id" :span="6" style="padding:10px;">
                   <el-card shadow="hover" style="font-size:16px;line-height: 20px;">
                     <i class="el-icon-star-off"></i>
-                    <a>{{item.name}}</a><br/>
+                    <a style="font-weight: bold;">{{item.name}}</a><br/>
                     <i class="el-icon-message"></i>
-                    <a :href="item.url" target="_blank" style="text-decoration: none; color: deepskyblue;">TA的主页</a><br/>
-                    <img :src="item.avatar" style="width:100%;height:100%">
+                    <a :href="item.url" target="_blank" style="text-decoration: none; color: #409EFF;">TA的主页</a><br/>
+                    <img :src="item.avatar" style="width:90%;height:90%; padding: 5px;border-radius: 10%; margin-top: 5%;">
                   </el-card>
                 </el-col>
               </el-row>
@@ -21,10 +21,10 @@
             </div>
           </el-tab-pane>
 
-          <el-tab-pane label="粉丝">
-            <div v-if="follower.length">
+          <el-tab-pane :label="'粉丝 ' + follower.total">
+            <div v-if="follower.list.length" v-loading="follower.loading">
               <el-row style="min-height: 200px;" :gutter="20">
-                <el-col v-for="item in follower" :key="item.id" :span="6" style="padding:10px;">
+                <el-col v-for="item in follower.list" :key="item.id" :span="6" style="padding:10px;">
                   <el-card shadow="hover" style="font-size:16px;line-height: 20px;">
                     <i class="el-icon-star-off"></i>
                     <a>{{item.name}}</a><br/>
@@ -35,7 +35,7 @@
                 </el-col>
               </el-row>
             </div>
-            <div v-else>
+            <div style="min-height: 300px;margin-bottom: 20px;padding: 20px 0px 20px 0px;text-align: center" v-else>
               <b>暂无粉丝</b>
             </div>
           </el-tab-pane>
@@ -50,25 +50,36 @@ import user from '@/api/user'
   export default {
     data() {
       return {
-        follower:[],
-        following: []
+        follower: {
+          list: [],
+          loading: false,
+          total: 0
+        },
+        following: {
+          list: [],
+          loading: false,
+          total: 0
+        },
       }
     },
     methods:{
       fetchFollower(){
+        this.follower.loading = true
         user.followers().then(data => {
+          this.follower.total = data.length
           for(let i = 0; i < data.length; i++){
-            this.follower.push({id: data[i].id, name: data[i].login, avatar: data[i].avatar_url, url: data[i].html_url})
+            this.follower.list.push({id: data[i].id, name: data[i].login, avatar: data[i].avatar_url, url: data[i].html_url})
           }
-        })
+        }).then( () => this.follower.loading = false )
       },
       fetchFollowing(){
+        this.following.loading = true
         user.following().then(data =>{
-          console.log(data)
+          this.following.total = data.length
           for(let i = 0; i < data.length; i++){
-            this.following.push({id: data[i].id, name: data[i].login, avatar: data[i].avatar_url, url: data[i].html_url})
+            this.following.list.push({id: data[i].id, name: data[i].login, avatar: data[i].avatar_url, url: data[i].html_url})
           }
-        })
+        }).then(() =>  this.following.loading = false)
       }
     },
     mounted(){
