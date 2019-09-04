3e552a7d1c9f9f4d59a4fb0447818b63a456b549
Shyee-wsy <shyee-wsy@outlook.com>
2019-09-04 22:39:45 +0800

    注释

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,10 +2,10 @@ const getters = {
   token: state => state.token.token,
   githubUsername: state => state.configuration.githubUsername,
   htmlTile: state => state.configuration.htmlTile,
-  avatarUrl: state => state.user.avatarUrl,
+  // avatarUrl: state => state.user.avatarUrl,
   name: state => state.user.name,
   blog: state =>state.user.blog,
-  followersTotal: state => state.user.followers,
-  followingTotal: state => state.user.following
+  // followersTotal: state => state.user.followers,
+  // followingTotal: state => state.user.following
 }
 export default getters
