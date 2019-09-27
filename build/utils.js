00135a3d494045d8baf45e638b9251cc867a0e35
Shyee-wsy <shyee-wsy@outlook.com>
2019-09-27 18:40:23 +0800

    解决打包后找不到图标库的问题

--- build/utils.js
+++ build/utils.js
@@ -47,7 +47,8 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader',
+        publicPath: '../..' // 解决找不到图标库的问题
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
