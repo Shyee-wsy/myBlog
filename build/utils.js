73add3cf37dac99c736c16ecc9ee3326b94ec5f3
Shyee-wsy <shyee-wsy@outlook.com>
2019-09-27 22:44:24 +0800

    处理打包后图标路径

--- build/utils.js
+++ build/utils.js
@@ -48,7 +48,7 @@ exports.cssLoaders = function (options) {
       return ExtractTextPlugin.extract({
         use: loaders,
         fallback: 'vue-style-loader',
-        publicPath: '../..' // 解决找不到图标库的问题
+        publicPath: '../../' // 解决找不到图标库的问题
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
